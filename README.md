7d2019dbe0759e24bf32de6a6ed44d22695e4a7b
nathanamacleod <nathanamacleod@penguin>
2020-06-24 22:12:19 +0100

    Changed priorities to focus on the more important stages of development

--- README.md
+++ README.md
@@ -4,4 +4,18 @@ A terminal shell environment with support for a few other programs written in Py
 When you run the program, it launches you straight into the app launcher, where you can run many apps (most of them aren't completed yet), including a terminal shell, calendar, file browser, task monitor, etc.
 
 # Contributing
-I am looking for contributers to help with this project, with things like the applications (or anything else you would like to help with), because there are quite a few of them to do.
+I am looking for contributers to help with this project, especially with things like the applications (or anything else you would like to help with), because there are quite a few of them to do.
+
+## Here is a list of apps, and whether they have been completed:
+* Terminal Shell (Mostly Completed)
+* Text Editor (Not Started)
+* File Browser (Not Started)
+* Games Library (Not Started)
+* Settings (Not Started)
+
+## Here is a list of potential ideas for the future:
+* Calendar
+* Web Browser
+* Email Client
+* Task Monitor
+* Development Library (Not sure how to do it yet, but maybe a python library (canvas.py or something to link up to automatically) where you can create graphical programs with the program.)
--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 from canvas import *  # Internal canvas.py file for ascii windows and stuff
-import webBrowser     # webBrowser.py file
 import datetime       # For printing the time at the terminal shell
 import sys            # For exiting the program (sys.exit)
-import psutil         # For the task manager
-import time
 
 version = "0.1"
 commandHistory = []
@@ -36,29 +33,15 @@ def terminalShell():
         except:
             pass
 
-def taskMonitor():
-    os.system("clear")
-    print("CPU Percent(s):", *psutil.cpu_percent(percpu=True), sep="\t")
-    option = ""
-
-    while option != "q":
-        os.system("clear")
-        print("CPU Percent(s):", *psutil.cpu_percent(percpu=True), sep="\t")
-        option = input("Press 'q' <ENTER> to quit this program, or just <ENTER> to refresh: ")
-
 def appLauncher():
     canv.addWindow(0, 0, width-1, height-3, "A P P   L A U N C H E R  -  VERSION " + version, False)
     canv.addStr(2, 2, appGlyph + " 1. Terminal Shell")
     canv.addStr(2, 3, appGlyph + " 2. Text Editor")
-    canv.addStr(2, 4, appGlyph + " 3. Calendar")
-    canv.addStr(2, 5, appGlyph + " 4. File Browser")
-    canv.addStr(2, 6, appGlyph + " 5. Web Browser")
-    canv.addStr(2, 7, appGlyph + " 6. Email Client")
-    canv.addStr(2, 8, appGlyph + " 7. Games Library")
-    canv.addStr(2, 9, appGlyph + " 8. Task Monitor")
-    canv.addStr(2, 10, appGlyph + " 9. Settings")
+    canv.addStr(2, 4, appGlyph + " 3. File Browser")
+    canv.addStr(2, 5, appGlyph + " 4. Games Library")
+    canv.addStr(2, 6, appGlyph + " 5. Settings")
     # Development Library Environment. # (Not sure how to do it yet, but maybe a python library (canvas.py or something to link up to automatically) where you can create graphical programs the the program.)
-    canv.addStr(2, 12, "10. EXIT")
+    canv.addStr(2, 8, "6. EXIT")
 
     # Colors for heading and window content
     canv.addPixel(0, 0, "\033[44m", "l")
@@ -69,19 +52,13 @@ def appLauncher():
     option = input(inputGlyph + "Please enter an option (1-9): ")
 
     # If it's an invalid option, run the function again
-    if option not in ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"]:
+    if option not in ["1", "2", "3", "4", "5", "6"]:
         appLauncher()
 
     elif option == "1":
         terminalShell()
 
-    elif option == "5":
-        webBrowser.main()
-
-    elif option == "8":
-        taskMonitor()
-    
-    elif option == "10":
+    elif option == "6":
         sys.exit() # Just ends the whole program
 
     appLauncher() # Once it has run a program, it goes back to the main home screen (app launcher screen)
