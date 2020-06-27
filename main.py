ae4176dc3beeed2fe47a35ad3e029f6b1762d119
nathanamacleod <nathanamacleod@penguin>
2020-06-27 21:33:35 +0100

    Improved bootup message and added default software planner text

--- main.py
+++ main.py
@@ -136,12 +136,14 @@ def appLauncher(stdscr):
 
 # Something containing the version number, and maybe some ascii art (logo or something) - then it says to press any key to continue.
 def bootupScreen(stdscr):
-    bootupMessage = "Welcome to version " + version + " of this terminal shell/desktop environment/window manager. Press any key to continue."
+    bootupMessage1 = "Version " + version
+    bootupMessage2 = "Press any key to continue"
     
     curses.init_pair(1, curses.COLOR_WHITE, curses.COLOR_BLUE)
 
     stdscr.bkgd(" ", curses.color_pair(1))
-    stdscr.addstr(curses.LINES//2, curses.COLS//2-len(bootupMessage)//2, bootupMessage)
+    stdscr.addstr(curses.LINES//2-1, curses.COLS//2-len(bootupMessage1)//2, bootupMessage1)
+    stdscr.addstr(curses.LINES//2, curses.COLS//2-len(bootupMessage2)//2, bootupMessage2)
 
     stdscr.getch()
     curses.endwin()
