a35bd47ddf80221186ed157cc529da1ec3a8dd68
nathanamacleod <nathanamacleod@penguin>
2020-06-28 11:27:03 +0100

    Added separated to software planner to separate textbox from title

--- main.py
+++ main.py
@@ -61,14 +61,23 @@ def softwarePlanner(stdscrRoot):
     stdscr.bkgd(" ", curses.color_pair(3))
     stdscr.border()
     stdscr.addstr(1, 1, "Welcome to the software planner - why not plan out some software in here? Press Ctrl-G to exit.")
+    for i in range(0, curses.COLS):
+        if i == 0:
+            stdscr.addstr(2, 0, "\u251c")
+
+        elif i == curses.COLS-1:
+            stdscr.addstr(2, i, "\u2524")
+
+        else:
+            stdscr.addstr(2, i, "\u2500")
     stdscr.refresh()
 
-    editWin = curses.newwin(curses.LINES-4, curses.COLS-2, 3, 1)
+    editWin = curses.newwin(curses.LINES-5, curses.COLS-2, 4, 1)
     editWin.bkgd(" ", curses.color_pair(3))
 
     with open("softwarePlannerText.txt", "r") as file:
         for i, line in enumerate(file):
-            editWin.addstr(i+2, 0, line)
+            editWin.addstr(i, 0, line)
     file.close()
 
     box = Textbox(editWin)
