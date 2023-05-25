f4f30d7e437032b8359b120c54c0ab3365902f64
RobiTheGit <94720060+RobiTheGit@users.noreply.github.com>
2023-05-25 16:26:02 -0600

    read the background color from a file

--- src/main.py
+++ src/main.py
@@ -3,25 +3,48 @@
 import curses
 import os
 import random
+import re
 # ...and some other files
 from CoreLib.Windows.windowClass import *
 from CoreLib.screenCycle import *
+from pathlib import Path
+bgclr = Path("bgclr.txt")
+if bgclr.exists():
+    f = open("bgclr.txt", "r")
+else:
+    os.system("touch bgclr.txt")
+    f = open("bgclr.txt", "w")
+    f.write("blue")
+    f.close()
+    f = open("bgclr.txt", "r")  
+x = f.read()
+bgnd = re.sub("\n", '', x).lower()
+colors = {
+"blue" : curses.COLOR_BLUE,
+"black" : curses.COLOR_BLACK,
+"cyan" : curses.COLOR_CYAN,
+"green" : curses.COLOR_GREEN,
+"magenta" : curses.COLOR_MAGENTA,
+"red" : curses.COLOR_RED,
+"white" : curses.COLOR_WHITE,
+"yellow" : curses.COLOR_YELLOW
+}
 
-blue = curses.COLOR_BLUE
-black = curses.COLOR_BLACK
-cyan = curses.COLOR_CYAN
-green = curses.COLOR_GREEN
-magenta = curses.COLOR_MAGENTA
-red = curses.COLOR_RED
-white = curses.COLOR_WHITE
-yellow = curses.COLOR_YELLOW
+blue = colors["blue"]
+black = colors["black"]
+cyan = colors["cyan"]
+green = colors["green"]
+magenta = colors["magenta"]
+red = colors["red"]
+white = colors["white"]
+yellow = colors["yellow"]
 
 # The main function
 def main(stdscr):
     # Color combinations
     curses.init_pair(1, black, yellow) # For the shadows
     curses.init_pair(2, black, white) # Same, but inverted
-    curses.init_pair(3, white, blue) # The background color
+    curses.init_pair(3, white, colors[bgnd]) # The background color
     curses.init_pair(4, white, black) # For the titles
     # Stdscr settings
     curses.curs_set(0)
