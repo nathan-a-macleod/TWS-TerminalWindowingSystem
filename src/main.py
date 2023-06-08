f7389b7f424633aec443c347eb41d7387641fa7e
RobiWanKenobi <94720060+RobiTheGit@users.noreply.github.com>
2023-06-08 14:42:19 -0600

    Improvement to Welcome Screen
    Added a small bit of code to automatically position and draw the lines based off of a dictionary
    Will not crash when drawing the screen for this (the desktop still will crash if you have too small of a screen or too big of a font)
    Improved the commenting of the code

--- src/main.py
+++ src/main.py
@@ -1,42 +1,67 @@
 #!/usr/bin/python3
 # Import the curses library...
 import curses
+# Import some other libraries
 import os
 import random
 import re
-# ...and some other files
+from pathlib import Path
+# ...and some other files to deal with managing windows
 from CoreLib.Windows.windowClass import *
 from CoreLib.screenCycle import *
-from pathlib import Path
 from CoreLib.setcolor import SetThemeColors
-
 def main(stdscr):
     SetThemeColors()
-    
-    # Stdscr settings
+
+# Stdscr settings
+
     curses.curs_set(0)
     stdscr.bkgd(" ", curses.color_pair(3))
     stdscr.refresh()
-    # The bootup screen:
-    line0 = "╔═══════════════════════════════════════════════════════╗"
-    line1 = "║       Welcome to TWS-TerminalWindowingSystem!         ║"
-    line2 = "║To access help at any time, press '?' on your keyboard.║"
-    line3 = "║           Press any key to continue...                ║"
-    line4 = "╚═══════════════════════════════════════════════════════╝"
-    stdscr.addstr(curses.LINES//2-4, curses.COLS//2-len(line0)//2, line0)
-    stdscr.addstr(curses.LINES//2-3, curses.COLS//2-len(line1)//2, line1)
-    stdscr.addstr(curses.LINES//2-2, curses.COLS//2-len(line2)//2, line2)
-    stdscr.addstr(curses.LINES//2-1, curses.COLS//2-len(line3)//2, line3)
-    stdscr.addstr(curses.LINES//2, curses.COLS//2-len(line4)//2, line4)    
-    stdscr.getch()
+
+# The bootup screen:
+
+    LineDict = {
+    0 : "╔═══════════════════════════════════════════════════════╗",
+    1 : "║       Welcome to TWS-TerminalWindowingSystem!         ║",
+    2 : "║To access help at any time, press '?' on your keyboard.║",
+    3 : "║           Press any key to continue...                ║",
+    4 : "╚═══════════════════════════════════════════════════════╝"
+    }
+
+# A bunch of math to figure out where to start the line at so the text is centered, no matter what, unless it is above the top or bottom of the screen
+ 
+    scrline = int(round((((len(LineDict)/2)-1)*-1), 0))
+    txtline = 0
+
+#The Welcome Message Drawing Code
+
+    for lines in range(len(LineDict)): #Run for as many lines there are in the welcome screen
+        #(Y,X,Text)
+        #Add the text of LineDict[txtline] to the screen     
+        try:
+            stdscr.addstr(
+        curses.LINES//2+scrline, #Y
+        curses.COLS//2-len(LineDict[txtline])//2,#X
+        LineDict[txtline] #Text
+        )
+        except:
+            break
+
+        scrline += 1 # make the screen go down a line for printing the next line
+        txtline += 1 # tell us to read from a different index of LineDict
+#Screen refresh and set timeout
+    stdscr.getch() #refresh the screen and wait for the user to hit a key
     stdscr.timeout(500) # Set the timeout from now on
 
-    # The core of the program
+# The core of the program
+
     scr = Screen(stdscr)
     scr.mainloop()
 
-    # Update the screen and wait for 1 second (curses.napms())
-    stdscr.refresh()
-    curses.napms(1000)
+# Update the screen and wait for 1 second (curses.napms())
 
-curses.wrapper(main)
+    stdscr.refresh()
+    curses.napms(1000) # 1 second = 1000 milliseconds
+
+curses.wrapper(main) #Start Curses from the Main function
