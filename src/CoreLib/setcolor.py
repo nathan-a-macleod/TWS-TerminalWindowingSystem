8afd2124aa017802472cca79365e147e465ea64b
RobiWanKenobi <94720060+RobiTheGit@users.noreply.github.com>
2023-06-10 15:47:22 -0600

    Improved some of the setcolor routines

--- src/CoreLib/setcolor.py
+++ src/CoreLib/setcolor.py
@@ -1,13 +1,15 @@
+#	Import Curses and some other modules
 import curses
 import os
 import random
 import re
-# ...and some other files
+#	Import the screen managing code
 from CoreLib.Windows.windowClass import *
 from CoreLib.screenCycle import *
 from pathlib import Path
 def SetThemeColors():
-    bgclr = Path("config.cfg") #Note, the color has to be one of the 7 default colors
+#	Note, the color has to be one of the 7 default colors because of curses
+    bgclr = Path("config.cfg")
     if bgclr.exists():
         f = open("config.cfg", "r")
     else:
@@ -15,11 +17,18 @@ def SetThemeColors():
         f = open("config.cfg", "w")
         f.write("COLOR:blue\nTHEME:light")
         f.close()
+
+#	Set The Variable Colors
     f = open("config.cfg", "r")
-    bgnd3 = f.readlines()
-    bgnd2 = bgnd3[1]
+    config = f.readlines()
+    bgnd2 = config[1]
     bgnd1 = re.sub("COLOR:", '', bgnd2).lower()
     bgnd = re.sub("\n", '', bgnd1).lower()
+    thm2 = config[2]
+    thm1 = re.sub("THEME:", '', thm2).lower()
+    mode = re.sub("\n", '', thm1).lower()
+
+#	Dictionary for setting colors since we use the config to get the color and therefore, we can't actually use the text as a variable without this   
     colors = {
 "blue" : curses.COLOR_BLUE,
 "black" : curses.COLOR_BLACK,
@@ -31,14 +40,14 @@ def SetThemeColors():
 "yellow" : curses.COLOR_YELLOW
 }
 
-    blue = colors["blue"] #Can have white text
-    black = colors["black"] #Can have white text
-    cyan = colors["cyan"] #Can't have white text (readability)
-    green = colors["green"] #Can have white text
-    magenta = colors["magenta"] #Can't have white text (readability)
-    red = colors["red"] #Can have white text
-    white = colors["white"] #Can't have white text (readability)
-    yellow = colors["yellow"] #Can't have white text (readability)
+    blue = colors["blue"]	#	Can have white text
+    black = colors["black"]	#	Can have white text
+    cyan = colors["cyan"]	#	Can't have white text for the sake of being readable
+    green = colors["green"]	#	Can have white text
+    magenta = colors["magenta"]	#	Can't have white text for the sake of being readable
+    red = colors["red"]		#	Can have white text
+    white = colors["white"]	#	Can't have white text for the sake of being readable
+    yellow = colors["yellow"]	#	Can't have white text for the sake of being readable
 
     BlackFGColors = [
 "white",
@@ -46,34 +55,34 @@ def SetThemeColors():
 "cyan",
 "magenta"
 ]
-    f2 = open("config.cfg", "r")
-    thm3 = f2.readlines()
-    thm2 = thm3[2]
-    thm1 = re.sub("THEME:", '', thm2).lower()
-    mode = re.sub("\n", '', thm1).lower()
-    
-    if bgnd in BlackFGColors:
+
+
+    if bgnd in BlackFGColors:	#If we would have a hard time seeing the color white on the background
         fg = "black"
     else:
         fg = "white"
-    
+    Foreground = colors[fg]
+    Background = colors[bgnd]        
+#	For the shadows on the text
     if bgnd == "yellow":
-        curses.init_pair(1, black, red) # For the shadows       
+        curses.init_pair(1, black, red)  
     else:
-        curses.init_pair(1, black, yellow) # For the shadows
-             
-    if mode == "dark":    
-        curses.init_pair(2, white, black) # Same, but inverted, Apps Background and Foreground Colors
-    else:
-        curses.init_pair(2, black, white) # Same, but inverted, Apps Background and Foreground Colors
-        
-    try:
-        curses.init_pair(3, colors[fg], colors[bgnd]) # The background color
-    except:
-        curses.init_pair(3, white, blue) # The background color  
-             
-    if mode == "dark":         
-        curses.init_pair(4, black, white) # Same, but inverted, Apps Background and Foreground Colors
-    else:    
-        curses.init_pair(4, white, black) # Same, but inverted, Apps Background and Foreground Colors
+        curses.init_pair(1, black, yellow)
 
+#	Apps Background and Foreground Colors             
+    if mode == "dark":    
+        curses.init_pair(2, white, black)
+    else:
+        curses.init_pair(2, black, white) 
+
+#	The Desktop Background and Text Color        
+    try:
+        curses.init_pair(3, Foreground, Background)
+    except:
+        curses.init_pair(3, white, blue)
+
+#	Taskbar Alternate Color             
+    if mode == "dark":         
+        curses.init_pair(4, black, white)
+    else:    
+        curses.init_pair(4, white, black)
